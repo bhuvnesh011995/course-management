31cc1e79eaa1aff334ec10118a2b23a6a0f546cd
Saurav <sauravamin@braincavesoft.com>
2023-08-26 13:03:10 +0530

    m

---
+++ assets/js/pages/task-kanban.init.js
@@ -0,0 +1,6 @@
+dragula([
+    document.getElementById("lead-column-1"),
+    document.getElementById("lead-column-2"),
+    document.getElementById("lead-column-3"),
+    document.getElementById("lead-column-4")
+]);
