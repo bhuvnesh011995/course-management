5b8b3a5f2a0b175c3e8f02a91138eca0ac961959
bhuvnesh <bhuvnesh@braincavesoft.com>
2023-11-20 17:02:21 +0530

    ;

--- server/index.js
+++ server/index.js
@@ -8,7 +8,10 @@ const app = express();
 require("dotenv").config();
 
 require("./configs/database.config");
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.static("uploads"));
